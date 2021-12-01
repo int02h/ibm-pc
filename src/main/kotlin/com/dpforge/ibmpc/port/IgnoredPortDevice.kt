36afe76e189c7be500034906851d9d4e94f36c9e
Daniil Popov <int02h@gmail.com>
2021-12-01 22:33:38 +0200

    Ignore port 0x72

--- src/main/kotlin/com/dpforge/ibmpc/port/IgnoredPortDevice.kt
+++ src/main/kotlin/com/dpforge/ibmpc/port/IgnoredPortDevice.kt
@@ -7,7 +7,8 @@ class IgnoredPortDevice : PortDevice {
     private val logger = LoggerFactory.getLogger("IgnoredPortDevice")
 
     override fun getPortMapping(): Map<Int, Port> = mapOf(
-        0x2c1 to IgnoredPort("AST-clock (probably)", logger)
+        0x2c1 to IgnoredPort("AST-clock (probably)", logger),
+        0x72 to IgnoredPort("Chips&Technologies 82C100 - NMI CONTROL", logger),
     )
 
 }
